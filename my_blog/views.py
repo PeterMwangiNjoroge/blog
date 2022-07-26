3190ffd02ab42406e6c4f3ffb4bfa46e515be0bc
Peter Mwangi <petermaara2@yahoo.com>
2022-07-26 19:19:28 +0300

    fix post list view to put new blog post first!

--- my_blog/views.py
+++ my_blog/views.py
@@ -10,7 +10,7 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(
-        published_date__lte=timezone.now()).order_by('published_date')
+        published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'my_blog/post_list.html', {'posts': posts})
 
 
